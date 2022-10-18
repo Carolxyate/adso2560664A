5cecd878525829aa06aebc20ccefbddefd0b48fa
carolm <carolyate@gmail.com>
2022-10-18 16:33:59 -0500

    version3.2

---
+++ ejersegundos.py
@@ -0,0 +1,8 @@
+tiempo = input("Ingrese el valor en segundos: ")
+s = float(tiempo)
+m = s / 60
+h = m / 60
+print("Convertidos en horas: ", h)
+print("Convertidos en minutos: ", m)
+print("Convertidos en segundos: ", s)
+
