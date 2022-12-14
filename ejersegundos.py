43c5d4d232573f9bb396214b06dec51f8b7ee737
Carolm <116192237+Carolxyate@users.noreply.github.com>
2022-12-14 08:44:09 -0500

    Delete ejersegundos.py

--- ejersegundos.py
+++
@@ -1,9 +0,0 @@
-Segundo = int(input("ingrese valor en segundos"))
-minuto= (Segundo/60)
-hora= (minuto/60)
-print ("Segundos:", Segundo)
-if (minuto):
-    print ("Minutos:",minuto)
-if (hora):
-    print("Horas:",hora)
-
