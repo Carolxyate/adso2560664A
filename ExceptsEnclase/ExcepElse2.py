cc7bda6ade5116e3ff2cb4fb9f77baec3c301889
Carolm <116192237+Carolxyate@users.noreply.github.com>
2023-02-24 10:55:46 -0500

    Add files via upload

---
+++ ExceptsEnclase/ExcepElse2.py
@@ -0,0 +1,14 @@
+def multiplos():
+    num = input("Ingrese un número entero: ")
+    
+    try:
+        num = int(num)
+        for i in range(1, 11):
+            print(num * i)
+    except ValueError:
+        print("Debe ingresar un número entero. Intente nuevamente.")
+    else:
+        print('el resultado es: ',num)
+    finally:
+        print('exiting')
+multiplos()
