d93226dc2a2c93c3207b8b837a1fbc95960536b3
Carolm <116192237+Carolxyate@users.noreply.github.com>
2023-03-01 07:45:02 -0500

    Update clases.py

--- Comentarios/clases.py
+++ Comentarios/clases.py
@@ -1,3 +1,4 @@
+#intento documento
 """class Persona:# se crea la clase persona
     def __init__(self,nombre,docu):#se define la funcion con el construrtor y un parametro
         self.__nombre=nombre#atributo partuckar oar saber  
@@ -38,3 +39,33 @@ app=Aprendiz('Pedro',12345)
 print(app.getFicha())
 print(app.getNombre())
 print(app.getdocu())"""
+
+
+class Persona:# se crea la clase persona
+    def __init__(self,nombre):#se define la funcion con el construrtor y un parametro
+        self.__nombre=nombre#se une con el atributo nombre
+        #print('Constructor Activado')        
+
+    def getNombre(self): #se define la funcion con el metodo get para acceder al valor del atributo.
+        return self.__nombre
+
+    def setNombre(self,nombre):#se define la funcion con el metodo set para acceder al atributo
+        self.__nombre=nombre
+
+ob=Persona('Maria')
+print(ob.getNombre())
+ob.setNombre('Ana')
+print(ob.getNombre())
+#print(type(ob))
+
+class Aprendiz(Persona):#se crea una subclase con la clase padre que es persona
+    def __init__(self,nombre,ficha):#se define la funcion con el construrtor y un parametro
+        Persona.__init__(self,nombre)
+        self.__ficha=ficha#se une con el atributo nombre
+
+    def getFicha(self):
+        return self.__ficha
+
+app=Aprendiz('Pedro',12345)
+print(app.getFicha())
+print(app.getNombre())
